6e910747a1dec25a1c2ee692205eb8de96de9447
Toma Stefan <stefan.toma@stud.ubbcluj.ro>
2024-11-04 09:41:21 +0200

    refactor: changed the logic of the program for the hardcoded exemples

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,18 @@
 package controller;
 
 import exceptions.*;
-import model.adts.MyIStack;
-import model.statements.IStatement;
+import model.adts.*;
+import model.expressions.ArithmeticalExpression;
+import model.expressions.ArithmeticalOperator;
+import model.expressions.ValueExpression;
+import model.expressions.VariableExpression;
+import model.statements.*;
 import model.states.PrgState;
+import model.types.BoolType;
+import model.types.IntType;
+import model.values.BoolValue;
+import model.values.IValue;
+import model.values.IntValue;
 import repo.IRepository;
 
 public class Controller implements IController{
@@ -26,14 +35,53 @@ public class Controller implements IController{
         this.repo.addPrgState(prgState);
     }
 
-    public void allSteps() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException {
+    public MyIList<String> allSteps() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException {
         PrgState prgState = this.repo.getCurrentPrgState();
-        System.out.println(prgState.toString());
+        //System.out.println(prgState.toString());
         while(!prgState.getExecStack().isEmpty()){
             oneStep(prgState);
-            System.out.println("-------------------------------------------------------------------");
-            System.out.println(prgState.toString());
+            //System.out.println("-------------------------------------------------------------------");
+            //System.out.println(prgState.toString());
 
         }
+        return prgState.getOutputList();
+    }
+
+    @Override
+    public MyIList<String> prg1() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException {
+        IStatement st = new CompStatement(new VarDeclStatement("v", new IntType()),
+                new CompStatement(new AssignStatement("v",
+                        new ValueExpression(new IntValue(2))), new PrintStatement(new VariableExpression("v"))) );
+
+        PrgState initPrgState = new PrgState(st, new MyStack<IStatement>(), new MyMap<String, IValue>(), new MyList<String>());
+        add(initPrgState);
+        return allSteps();
+    }
+
+    @Override
+    public MyIList<String> prg2() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException {
+        IStatement st = new CompStatement(new VarDeclStatement("a", new IntType()),
+                new CompStatement(new VarDeclStatement("b", new IntType()), new CompStatement(new AssignStatement("a",
+                        new ArithmeticalExpression(new ValueExpression(new IntValue(2)), new ArithmeticalExpression(new ValueExpression(new IntValue(3)), new ValueExpression(new IntValue(5)), ArithmeticalOperator.MULTIPLY), ArithmeticalOperator.ADD)
+                ),new CompStatement(new AssignStatement("b", new ArithmeticalExpression(new VariableExpression("a"), new ValueExpression(new IntValue(1)), ArithmeticalOperator.ADD)),
+                        new PrintStatement(new VariableExpression("b"))))
+                ));
+
+        PrgState initPrgState = new PrgState(st, new MyStack<IStatement>(), new MyMap<String, IValue>(), new MyList<String>());
+        add(initPrgState);
+        return allSteps();
+    }
+
+    @Override
+    public MyIList<String> prg3() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException {
+        PrintStatement pr = new PrintStatement(new VariableExpression("v"));
+        CompStatement cm4 = new CompStatement(new IfStatement(new VariableExpression("a"), new AssignStatement("v", new ValueExpression(new IntValue(2))), new AssignStatement("v", new ValueExpression(new IntValue(3)))), pr);
+        CompStatement cm3 = new CompStatement(new AssignStatement("a", new ValueExpression(new BoolValue(true))), cm4);
+        CompStatement cm2 = new CompStatement(new VarDeclStatement("v", new IntType()), cm3);
+        CompStatement cm1 = new CompStatement(new VarDeclStatement("a", new BoolType()), cm2);
+
+        PrgState initPrgState = new PrgState(cm1, new MyStack<IStatement>(), new MyMap<String, IValue>(), new MyList<String>());
+        add(initPrgState);
+        return allSteps();
     }
 }
--- src/controller/IController.java
+++ src/controller/IController.java
@@ -1,11 +1,16 @@
 package controller;
 
 import exceptions.*;
+import model.adts.MyIList;
+import model.adts.MyList;
 import model.statements.IStatement;
 import model.states.PrgState;
 
 public interface IController {
 
     void add(PrgState prgState);
-    void allSteps() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException;
+    MyIList<String> allSteps() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException;
+    MyIList<String> prg1() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException;
+    MyIList<String> prg2() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException;
+    MyIList<String> prg3() throws EmptyStackException, StatementException, ControllerException, KeyNotFoundException, ExpressionException;
 }
